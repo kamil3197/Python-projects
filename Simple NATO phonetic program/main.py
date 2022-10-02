701a3eec127c6d569d01be5fc17b39e90249491e
kamil3197 <105457421+kamil3197@users.noreply.github.com>
2022-10-02 10:57:16 +0200

    Delete main.py

--- Simple NATO phonetic program/main.py
+++
@@ -1,8 +0,0 @@
-import pandas
-pliczek = pandas.read_csv("nato_phonetic_alphabet.csv")
-dejta = pandas.DataFrame(pliczek)
-dane = dict(dejta.values)
-ask = input('provide a name').upper()
-name = [elements for elements in ask]
-for elements in name:
-    print(dane[elements])
